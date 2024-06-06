b7a714b125f32f2316bf79c835216de9a4b4573b
Sizan <Mahatosizan@gmail.com>
2024-06-06 20:59:44 +0545

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
+import 'package:e_com/utils/constants/colors.dart';
 import 'package:e_com/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
   runApp(const App());
-}
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme
-
-    );
-  }
 }
 
