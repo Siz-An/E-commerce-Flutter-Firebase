f5db50b988df55b7fbc7c823e6dbe4213103b59d
Sizan <Mahatosizan@gmail.com>
2024-06-08 20:35:45 +0545

    new one

---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,11 @@
+
+class TSizes{
+  // Padding and Margin Sizes
+  static const double xs = 4;
+  static const double sm = 8;
+  static const double md = 16;
+  static const double lg = 24;
+  static const double xl = 32;
+
+  // Icon Sizes
+}
