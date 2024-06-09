6b5f9d177fae9dfa1d49dbbab13b8b5f6cd46213
Sizan <Mahatosizan@gmail.com>
2024-06-09 23:11:17 +0545

    new one

---
+++ lib/utils/formatters/formatter.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class TFormatter{
+  static String formatDate(DateTime? date){
+    date ??= DateTime.now();
+    return DateFormat('dd-MM-yyyy').format(date);
+  }
+
+  static String formatCurrency(double amount)
+  {
+    return NumberFormat.currency(locale: 'en_US', symbol: '\$').format(amount);
+  }
+
+  static String formatPhoneNumber(String phoneNumber){
+    if(phoneNumber.length == 10){
+      return '(${phoneNumber.substring(0,3)}) ${phoneNumber.substring(3,6)} ${phoneNumber.substring(6)}';
+    }
+    else if(phoneNumber.length == 11){
+      return '(${phoneNumber.substring(0,4)}) ${phoneNumber.substring(4,7)} ${phoneNumber.substring(7)}';
+    }
+    return phoneNumber;
+  }
+
+}
---
+++ lib/utils/helpers/helper_function.dart
@@ -0,0 +1,88 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'dart:ui';
+
+class THelperFunction{
+  static Color? getColor(String value){
+    /// Define your product Specific Colors here and it will match the attribute colors
+
+    if (value == 'Green'){
+      return Colors.green;
+    }else if(value =='Green'){
+      return Colors.green;
+    }else if(value =='Red'){
+      return Colors.red;
+    }else if(value =='Blue'){
+      return Colors.blue;
+    }else if(value =='Pink'){
+      return Colors.pink;
+    }else if(value =='Grey'){
+      return Colors.grey;
+    }else if(value =='Black'){
+      return Colors.black;
+    }else if(value =='White'){
+      return Colors.white;
+    }else if(value =='Yellow'){
+      return Colors.yellow;
+    }else if(value =='Brown'){
+      return Colors.brown;
+    }else if(value =='Indigo'){
+      return Colors.indigo;
+    }else if(value =='Purple'){
+      return Colors.purple;
+    }else{
+      return null;
+    }
+  }
+  static void showSnackBar(String messages){
+    ScaffoldMessenger.of(Get.context!).showSnackBar(
+      SnackBar(content: Text(messages))
+    );
+  }
+  static void showAlert(String title, String message){
+    showDialog(
+        context: Get.context!,
+        builder: (BuildContext context){
+          return AlertDialog(
+            title: Text(title),
+            content: Text(message),
+            actions: [
+              TextButton(onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('OK') )
+            ],
+          );
+        });
+  }
+  static void navigateToScreen(BuildContext context, Widget screen){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (_) => screen)
+    );
+  }
+
+  static String truncateText(String text, int maxLength){
+    if(text.length <= maxLength){
+      return text;
+    }else{
+      return '${text.substring(0, maxLength)}...';
+    }
+  }
+
+  static bool isDarkMode(BuildContext context){
+    return Theme.of(context).brightness == Brightness.dark;
+  }
+
+  static Size screenSize(){
+    return MediaQuery.of(Get.context!).size;
+  }
+
+  static double screenHeight(){
+    return MediaQuery.of(Get.context!).size.height;
+  }
+
+  static double screenWidth(){
+    return MediaQuery.of(Get.context!).size.width;
+  }
+
+}
